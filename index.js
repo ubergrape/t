91dc2edfcedb1c01f3d275d36b7ef647be3a3326
Arpad Borsos <arpad.borsos@googlemail.com>
2014-03-26 11:53:17 +0100

    move to istanbul for coverage

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-module.exports = process.env.T_COV
-  ? require('./lib-cov')
-  : require('./lib');
+/* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
+"use strict";
+
+try {
+	exports = module.exports = require('t');
+} catch (e) {
+	exports = module.exports = require('t-component');
+}
+
+var definitions = {};
+
+exports.definition = function definition(code) {
+	if (typeof code === 'string')
+		return definitions[code];
+
+	// create registries registry
+	var parent = null;
+	if (code.inherits) {
+		parent = exports[code.inherits] = exports[code.inherits] || Object.create(null);
+	}
+	if (!exports[code.code])
+		exports[code.code] = Object.create(parent);
+	definitions[code.code] = code;
+};
+
+exports.merge = function merge(code, strings) {
+	var into = exports[code] = exports[code] || Object.create(null);
+	for (var key in strings)
+		into[key] = strings[key];
+};
+
+// load some standard definitions
+require('./lib/definitions').forEach(exports.definition);
